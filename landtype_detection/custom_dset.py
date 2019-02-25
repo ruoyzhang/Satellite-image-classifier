e6625f1316354d685d38f66a404ed1ea72a07bc4
Ruoy <ruoyzhang@gmail.com>
2019-02-25 21:52:48 +0100

    apparently more

--- landtype_detection/custom_dset.py
+++ landtype_detection/custom_dset.py
@@ -47,7 +47,7 @@ class custom_dset(Dataset):
             normalize = transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
             transform = transforms.Compose([
                 transforms.Resize(299),
-                transforms.CennterCrop(299),
+                transforms.CenterCrop(299),
                 transforms.ColorJitter(hue = .05, saturation = .05),
                 transforms.RandomHorizontalFlip(),
                 tramsforms.RandomVerticalFlip(),
