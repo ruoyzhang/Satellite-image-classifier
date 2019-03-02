187a8a26e492023679cafcaab34039a1abdef75a
Ruoy <ruoyzhang@gmail.com>
2019-03-03 00:39:08 +0100

    reduced traning epoch to 8

--- landtype_detection_alt/execute_training_alt.py
+++ landtype_detection_alt/execute_training_alt.py
@@ -9,7 +9,8 @@ save_dir = '../../data/CNN_model_landtype_alt/'
 num_class = 6
 bs = 8
 name = 'model_alt'
+num_epoch = 8
 
 
 # run
-loss_record, acc_record, model, test_data = train(data_dir = data_dir, save_dir = save_dir, num_class = 6, bs = bs, use_cuda = True)
+loss_record, acc_record, model, test_data = train(data_dir = data_dir, save_dir = save_dir, num_class = 6, num_epoch = num_epoch, bs = bs, use_cuda = True)
